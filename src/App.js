412341f54ca78ef8ff08073d1ac365b5f15bf9f1
Barry Porch <bporch@ymail.com>
2021-02-16 17:42:28 -0500

    Finished the MVP except for getting the likes click you work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 /* 
-  Start here and work your way down the nested components.
-  Not all files in the project need code added.
-  Look at each file to see what props need to be passed!
+  TODO: Start here and work your way down the nested components.
+  TODO: Not all files in the project need code added.
+  TODO: Look at each file to see what props need to be passed!
 */
 
 // *Import the state hook
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 // *Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // *Import the dummyData
@@ -16,10 +16,10 @@ import './App.css';
 
 const App = () => {
 // *Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
-// !This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
+// *This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
 // *To make the search bar work (which is stretch) we'd need another state to hold the search term.
   const [posts, setPosts] = useState(DummyData);
-  const [search, setSearch] = useState(SearchBar);
+  // const [search, setSearch] = useState(" ");
 
   const likePost = postId => {
     /*
@@ -29,14 +29,14 @@ const App = () => {
 
       *Invoke `setPosts` and pass as the new state the invocation of `posts.map`.
       *The callback passed into `map` performs the following logic:
-        !- if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
-        !- otherwise just return the post object unchanged.
+        *- if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
+        *- otherwise just return the post object unchanged.
      */
     setPosts(posts.map(post => {
-      // return post.postId === postId ? {...post, likes: likes + 1} : post
+      // return post.postId === postId ? {...post, likes: post.likes + 1} : post
 
       if(post.postId === postId){
-        return {...post, likes: likes + 1}
+        return {...post, likes: post.likes + 1}
       } else return post
     }))
   };
@@ -44,6 +44,8 @@ const App = () => {
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
+      <SearchBar />
+      <Posts likePost={likePost}posts={posts}/>
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
